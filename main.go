78df621785098ebb6e5105ee29c02ef5a9cbc2db
alekhya-kurmindla <alekhya.kurmindla@gmail.com>
2024-12-27 17:54:12 +0530

    custom terraform provider
    custom terraform provider

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"terraform-provider-hashicups/internal/provider"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+)
+var version = "dev"
+func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		// NOTE: This is not a typical Terraform Registry provider address,
+		// such as registry.terraform.io/hashicorp/hashicups. This specific
+		// provider address is used in these tutorials in conjunction with a
+		// specific Terraform CLI configuration for manual development testing
+		// of this provider.
+		Address: "hashicorp.com/edu/hashicups",
+		Debug:   debug,
+	}
+
+	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
